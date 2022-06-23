2ed9c8cf429cdeecfe8fc3595af316871023ab09
RafaLhp <82486043+RafaLhp@users.noreply.github.com>
2022-06-23 18:17:39 -0300

    Add files via upload

---
+++ Projeto_sqlds/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿
+namespace Projeto_sqlds
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCodigoTime = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblCodTime = new System.Windows.Forms.Label();
+            this.lblNomeTime = new System.Windows.Forms.Label();
+            this.txtNomeTime = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPaisTime = new System.Windows.Forms.TextBox();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnIncluir = new System.Windows.Forms.Button();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.cmbGrupoTime = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtCodigoTime
+            // 
+            this.txtCodigoTime.Location = new System.Drawing.Point(102, 21);
+            this.txtCodigoTime.Name = "txtCodigoTime";
+            this.txtCodigoTime.Size = new System.Drawing.Size(100, 23);
+            this.txtCodigoTime.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cmbGrupoTime);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtPaisTime);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtNomeTime);
+            this.groupBox1.Controls.Add(this.lblNomeTime);
+            this.groupBox1.Controls.Add(this.lblCodTime);
+            this.groupBox1.Controls.Add(this.txtCodigoTime);
+            this.groupBox1.Location = new System.Drawing.Point(12, 7);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(208, 203);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cadastramento do Time";
+            // 
+            // lblCodTime
+            // 
+            this.lblCodTime.AutoSize = true;
+            this.lblCodTime.Location = new System.Drawing.Point(6, 24);
+            this.lblCodTime.Name = "lblCodTime";
+            this.lblCodTime.Size = new System.Drawing.Size(96, 15);
+            this.lblCodTime.TabIndex = 1;
+            this.lblCodTime.Text = "Código do Time:";
+            // 
+            // lblNomeTime
+            // 
+            this.lblNomeTime.AutoSize = true;
+            this.lblNomeTime.Location = new System.Drawing.Point(6, 70);
+            this.lblNomeTime.Name = "lblNomeTime";
+            this.lblNomeTime.Size = new System.Drawing.Size(90, 15);
+            this.lblNomeTime.TabIndex = 2;
+            this.lblNomeTime.Text = "Nome do Time:";
+            // 
+            // txtNomeTime
+            // 
+            this.txtNomeTime.Location = new System.Drawing.Point(102, 67);
+            this.txtNomeTime.Name = "txtNomeTime";
+            this.txtNomeTime.Size = new System.Drawing.Size(100, 23);
+            this.txtNomeTime.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 116);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "País do Time:";
+            // 
+            // txtPaisTime
+            // 
+            this.txtPaisTime.Location = new System.Drawing.Point(102, 113);
+            this.txtPaisTime.Name = "txtPaisTime";
+            this.txtPaisTime.Size = new System.Drawing.Size(100, 23);
+            this.txtPaisTime.TabIndex = 5;
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(307, 186);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(75, 23);
+            this.btnExcluir.TabIndex = 2;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            // 
+            // btnIncluir
+            // 
+            this.btnIncluir.Location = new System.Drawing.Point(388, 186);
+            this.btnIncluir.Name = "btnIncluir";
+            this.btnIncluir.Size = new System.Drawing.Size(75, 23);
+            this.btnIncluir.TabIndex = 3;
+            this.btnIncluir.Text = "Incluir";
+            this.btnIncluir.UseVisualStyleBackColor = true;
+            this.btnIncluir.Click += new System.EventHandler(this.btnIncluir_Click);
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Location = new System.Drawing.Point(469, 186);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(75, 23);
+            this.btnAlterar.TabIndex = 4;
+            this.btnAlterar.Text = "Alterar";
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            this.btnAlterar.Click += new System.EventHandler(this.btnAlterar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(567, 12);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(51, 39);
+            this.btnSair.TabIndex = 5;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 159);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Grupo do Time:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(226, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(331, 169);
+            this.listBox1.TabIndex = 6;
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Location = new System.Drawing.Point(548, 186);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(75, 23);
+            this.btnConsultar.TabIndex = 7;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(226, 186);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 8;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // cmbGrupoTime
+            // 
+            this.cmbGrupoTime.FormattingEnabled = true;
+            this.cmbGrupoTime.Location = new System.Drawing.Point(102, 156);
+            this.cmbGrupoTime.Name = "cmbGrupoTime";
+            this.cmbGrupoTime.Size = new System.Drawing.Size(100, 23);
+            this.cmbGrupoTime.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(630, 221);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnAlterar);
+            this.Controls.Add(this.btnIncluir);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCodigoTime;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtPaisTime;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNomeTime;
+        private System.Windows.Forms.Label lblNomeTime;
+        private System.Windows.Forms.Label lblCodTime;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnIncluir;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.ComboBox cmbGrupoTime;
+    }
+}
+
---
+++ Projeto_sqlds/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Projeto_sqlds
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection sqlCon = null;
+
+        private String strCon = "Data Source = LI03 - 13; Initial Catalog = ; User ID = sa; password=123456";
+
+        private String strSql = "";
+
+        SqlConnection objConect = null;
+
+        SqlConnection objCommand = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbGrupoTime.Items.Add("GrupoA");
+            cmbGrupoTime.Items.Add("GrupoB");
+            cmbGrupoTime.Items.Add("GrupoC");
+            cmbGrupoTime.Items.Add("GrupoD");
+
+
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            strSql = "select * from " + cmbGrupoTime.SelectedItem + "";
+
+            sqlCon = new SqlConnection(strCon);
+
+            SqlCommand comando = new SqlCommand(strSql, sqlCon);
+
+            sqlCon.Open();
+
+            SqlDataReader dr = comando.ExecuteReader();
+
+            if (dr.HasRows == true)
+            {
+                dr.Read();
+
+                listBox1.Items.Add(Convert.ToString(dr[""]));
+            }
+            else
+            {
+                MessageBox.Show("Registro não existe!");
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            txtCodigoTime.Clear();
+            txtNomeTime.Clear();
+            txtPaisTime.Clear();
+        }
+
+        private void btnIncluir_Click(object sender, EventArgs e)
+        {
+            strSql = "insert into " + cmbGrupoTime.SelectedItem + " (codigo_time, nome_time, pais_time) values (@codigo_time, @nome_time, @pais_time)";
+
+            sqlCon = new SqlConnection(strCon);
+
+            SqlCommand comando = new SqlCommand(strSql, strCon);
+
+            comando.Parameters.Add("@codigo_time", SqlDbType.Int).Value = txtCodigoTime.Text;
+            comando.Parameters.Add("@nome_time", SqlDbType.VarChar).Value = txtNomeTime.Text;
+            comando.Parameters.Add("@pais_time", SqlDbType.VarChar).Value = txtPaisTime.Text;
+
+            sqlCon.Open();
+
+            comando.ExecuteNonQuery();
+
+            MessageBox.Show("Time cadastrado com sucesso!");
+
+            sqlCon.Close();
+        }
+
+        private void btnAlterar_Click(object sender, EventArgs e)
+        {
+            strSql = "update " + cmbGrupoTime.SelectedItem + " set codigo_time = @codigo_time, nome_time = @nome_time, pais_time = @pais_time";
+
+            sqlCon = new SqlConnection(strCon);
+
+            SqlCommand comando = new SqlCommand(strSql, strCon);
+
+            comando.Parameters.Add("@codigo_time", SqlDbType.Int).Value = txtCodigoTime.Text;
+            comando.Parameters.Add("@nome_time", SqlDbType.VarChar).Value = txtNomeTime.Text;
+            comando.Parameters.Add("@pais_time", SqlDbType.VarChar).Value = txtPaisTime.Text;
+
+            sqlCon.Open();
+
+            MessageBox.Show("Time alterado com sucesso!");
+
+            sqlCon.Close();
+        }
+    }
+}
